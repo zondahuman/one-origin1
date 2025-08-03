ca78243929acf55b13fbce05c937ede52b271347
zondahuman <zondahuman@gmail.com>
2025-08-03 16:27:04 +0800

    add

--- src/test/java/com/abin/algo/leetcode/btree/L230.java
+++ src/test/java/com/abin/algo/leetcode/btree/L230.java
@@ -1,5 +1,7 @@
 package com.abin.algo.leetcode.btree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 /**
@@ -7,29 +9,46 @@ import java.util.Stack;
  * BST 的中序遍历结果是有序的（升序），所以用一个外部变量记录中序遍历结果第 k 个元素即是第 k 小的元素。
  */
 public class L230 {
-
     /**
      * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 小的元素（从 1 开始计数）。
      * @param root
      * @param k
      * @return
      */
-    public int kthSmallest(TreeNode root, int k) {
-        if(null == root)
-            return 0;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        int count = 0;
-        while(!stack.isEmpty()){
-            TreeNode temp = stack.pop();
-            if(null != temp.left){
-                stack.push(temp.left);
-            }
-            if(null != temp.right){
-                stack.push(temp.right);
-            }
-        }
+    public int kthSmallest(TreeNode root, int k){
+        kthSmallestRecursion(root, k);
+        return result;
+    }
 
+    int result = 0;
+    int rank = 0;
+    public void kthSmallestRecursion(TreeNode root, int k) {
+        if(null == root)
+            return;
+        kthSmallestRecursion(root.left, k);
+        rank++;
+        if(k == rank) {
+            result = root.val;
+            return;
+        }
+        kthSmallestRecursion(root.right,k);
+    }
+
+    public int kthSmallestLoop(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (null != root || !stack.isEmpty()) {
+            while (null != root) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            --k;
+            if (k == 0)
+                break;
+            root = root.right;
+        }
+        return root.val;
     }
 
 
