a9fca8a71a29dc1f8c7846169cfa66d3c1e26caf
zondahuman <zondahuman@gmail.com>
2025-08-05 14:29:57 +0800

    add

--- src/test/java/com/abin/algo/leetcode/list/L25.java
+++ src/test/java/com/abin/algo/leetcode/list/L25.java
@@ -17,7 +17,31 @@ public class L25 {
      * @return
      */
     public ListNode reverseKGroup(ListNode head, int k) {
+        if(null == head)
+            return null;
+        // 区间 [a, b) 包含 k 个待反转元素
+        ListNode a = head, b = head;
+        for (int i = 0; i <k ; i++) {
+            if( b == null) return head;
+            b = b.next;
+        }
+        ListNode newHead = reverse(a, b);
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
 
+    ListNode reverse(ListNode a, ListNode b){
+        ListNode pre,cur,nxt;
+        pre = null;
+        cur = a;
+        nxt = a;
+        while(cur != b){
+            nxt = cur.next;
+            cur.next = pre ;
+            pre = cur ;
+            cur = nxt;
+        }
+        return pre;
     }
 
 
