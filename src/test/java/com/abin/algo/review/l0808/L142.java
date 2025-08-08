2b94eff76505111270ad232c5860a421a9afcd36
zondahuman <zondahuman@gmail.com>
2025-08-08 14:37:50 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l0808/L142.java
@@ -0,0 +1,24 @@
+package com.abin.algo.review.l0808;
+
+/**
+ * 142. 环形链表 II
+ */
+public class L142 {
+
+    public ListNode detectCycle(ListNode head) {
+
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+
+}
