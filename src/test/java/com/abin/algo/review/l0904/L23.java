81283bcf418bacb1f8164ca80f3e83059415abe0
zondahuman <zondahuman@gmail.com>
2025-09-05 16:40:08 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l0904/L23.java
@@ -0,0 +1,47 @@
+package com.abin.algo.review.l0904;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 23. 合并 K 个升序链表
+ */
+public class L23 {
+
+    /**
+     * 给你一个链表数组，每个链表都已经按升序排列。
+     * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) return null;
+        ListNode dummy = new ListNode(-1), p = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length,(a,b)->(a.val-b.val));
+        for(ListNode node:lists){
+            if(null != node){
+                pq.add(node);
+            }
+        }
+        while(!pq.isEmpty()){
+            ListNode listNode = pq.poll();
+            p.next = listNode;
+            if(listNode.next != null)
+                pq.add(listNode.next);
+            p = p.next;
+        }
+        return dummy.next;
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+}
