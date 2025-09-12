16449e38b9253dff9667edf75c8369632d3930f7
zondahuman <zondahuman@gmail.com>
2025-09-12 21:27:43 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l0912/L143.java
@@ -0,0 +1,48 @@
+package com.abin.algo.review.l0912;
+
+import com.abin.algo.review.l0911.L142;
+
+import java.util.Stack;
+
+/**
+ * 143. 重排链表
+ */
+public class L143 {
+    /**
+     * 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+     * L0 → L1 → … → Ln - 1 → Ln
+     * 请将其重新排列后变为：
+     * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+     * 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * @param head
+     */
+    public void reorderList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode listNode = head;
+        while(null != listNode){
+            stack.push(listNode);
+            listNode = listNode.next;
+        }
+        listNode = head;
+        while(!stack.isEmpty()){
+            ListNode lastNode = stack.pop();
+            ListNode next = listNode.next;
+            if(lastNode == next || lastNode.next == next){
+                lastNode.next = null;
+                break;
+            }
+            listNode.next = lastNode;
+            lastNode.next = next;
+            listNode = next;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
