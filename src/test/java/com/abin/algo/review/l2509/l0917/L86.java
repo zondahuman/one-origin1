781a630185b4a46de5eb97048fcc2ab9194ed240
zondahuman <zondahuman@gmail.com>
2025-09-17 18:09:00 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l2509/l0917/L86.java
@@ -0,0 +1,47 @@
+package com.abin.algo.review.l2509.l0917;
+
+import java.util.List;
+
+/**
+ * 86. 分隔链表
+ */
+public class L86 {
+
+    /**
+     * 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+     * 你应当 保留 两个分区中每个节点的初始相对位置。
+     * @param head
+     * @param x
+     * @return
+     */
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+        ListNode p1 = dummy1, p2 = dummy2;
+        ListNode p = head;
+        while(null != p){
+            if(p.val < x){
+                p1.next = p;
+                p1 = p1.next;
+            }else{
+                p2.next = p;
+                p2 = p2.next;
+            }
+            ListNode next = p.next;
+            p.next = null;
+            p = next;
+        }
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
