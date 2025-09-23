44cbad88ec625ad21341fee4a2ffb4283b9f346d
zondahuman <zondahuman@gmail.com>
2025-09-23 13:42:37 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l2509/l0923/L559.java
@@ -0,0 +1,44 @@
+package com.abin.algo.review.l2509.l0923;
+
+import java.util.List;
+
+/**
+ * 559. N 叉树的最大深度
+ */
+public class L559 {
+    /**
+     * 给定一个 N 叉树，找到其最大深度。
+     * 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+     * N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。
+     * @param root
+     * @return
+     */
+    public int maxDepth(Node root) {
+        if(null == root)return 0;
+        int result = 0;
+        for(Node node:root.children){
+            result = Math.max(result, maxDepth(node));
+        }
+        return 1+ result;
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+
+
+
+}
