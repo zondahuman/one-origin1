37c860b6947c008e1677f96c3c9f1412fc0eedf7
zondahuman <zondahuman@gmail.com>
2025-09-26 14:18:00 +0800

    add

--- src/test/java/com/abin/algo/review/l2509/l0925/L92.java
+++ src/test/java/com/abin/algo/review/l2509/l0925/L92.java
@@ -8,7 +8,8 @@ import com.abin.algo.review.l2509.l0903.L21;
 public class L92 {
 
     /**
-     * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+     * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+     * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
      * @param head
      * @param left
      * @param right
