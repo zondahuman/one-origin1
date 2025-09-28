ca2e89ce805d3f6f86656ca6e84d85d1a92e14bc
zondahuman <zondahuman@gmail.com>
2025-09-28 13:45:18 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/list/L1699ReverseBetween.java
@@ -0,0 +1,46 @@
+package com.abin.lee.algo.leetcode.questions.list;
+
+/**
+ * 1669. 合并两个链表
+ */
+public class L1699ReverseBetween {
+
+    /**
+     * 给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。
+     * 请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。
+     * @param list1
+     * @param a
+     * @param b
+     * @param list2
+     * @return
+     */
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode dummy1 = list1;
+        for (int i = 0; i <a-1 ; i++) {
+            dummy1 = dummy1.next;
+        }
+        ListNode dummy2 = dummy1;
+        for (int i = 0; i <b-a+2 ; i++) {
+            dummy2 = dummy2.next;
+        }
+        dummy1.next = list2;
+        while(null != list2.next){
+            list2 = list2.next;
+        }
+        list2.next = dummy2;
+
+        return list1;
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+}
---
+++ src/test/java/com/abin/algo/review/l2509/l0928/L92.java
@@ -0,0 +1,26 @@
+package com.abin.algo.review.l2509.l0928;
+
+import com.abin.algo.review.l2509.l0925.L1699;
+
+/**
+ * 92. 反转链表 II
+ */
+public class L92 {
+
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+
+}
