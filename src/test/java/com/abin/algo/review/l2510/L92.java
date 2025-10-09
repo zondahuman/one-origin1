629bb0f0897629c803464cddff3a4952fd210595
zondahuman <zondahuman@gmail.com>
2025-10-09 18:49:30 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l2510/L92.java
@@ -0,0 +1,36 @@
+package com.abin.algo.review.l2510;
+
+import com.abin.algo.review.l2509.l0930.L1669;
+
+/**
+ * 92. 反转链表 II
+ */
+public class L92 {
+
+    /**
+     * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+     * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+
+
+        return null;
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+}
