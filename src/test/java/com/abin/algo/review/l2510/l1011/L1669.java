0bb408635086e6a56e42455f9b818cd30b4ab3c9
zondahuman <zondahuman@gmail.com>
2025-10-11 19:36:18 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l2510/l1011/L1669.java
@@ -0,0 +1,31 @@
+package com.abin.algo.review.l2510.l1011;
+
+/**
+ * 1669. 合并两个链表
+ */
+public class L1669 {
+
+    /**
+     * 给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。
+     * 请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。
+     * @param list1
+     * @param a
+     * @param b
+     * @param list2
+     * @return
+     */
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+
+    }
+
+
+    
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
