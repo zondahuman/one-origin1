4d76849f142306976e2ff617d551815aad4241d3
zondahuman <zondahuman@gmail.com>
2025-10-22 19:13:53 +0800

    add

--- src/test/java/com/abin/algo/review/l2510/l1022/L300.java
+++ src/test/java/com/abin/algo/review/l2510/l1022/L300.java
@@ -1,5 +1,7 @@
 package com.abin.algo.review.l2510.l1022;
 
+import java.util.Arrays;
+
 /**
  * 300. 最长递增子序列
  */
@@ -13,8 +15,22 @@ public class L300 {
      * @return
      */
     public int lengthOfLIS(int[] nums) {
-
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i <nums.length ; i++) {
+            for (int j = 0; j <i ; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i <dp.length ; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        return result == Integer.MIN_VALUE ? 0 : result;
     }
+
 
 
 
