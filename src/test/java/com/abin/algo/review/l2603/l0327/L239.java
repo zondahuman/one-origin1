20c9cb64e1e3364bd5a7faa185c3b1be12fb52e7
zondahuman <zondahuman@gmail.com>
2026-03-27 14:58:33 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l2603/l0327/L239.java
@@ -0,0 +1,64 @@
+package com.abin.algo.review.l2603.l0327;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 239. 滑动窗口最大值
+ * https://leetcode.cn/problems/sliding-window-maximum/description/
+ */
+public class L239 {
+
+    /**
+     * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+     * 返回 滑动窗口中的最大值 。
+     *
+     * 示例 1：
+     * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+     * 输出：[3,3,5,5,6,7]
+     * 解释：
+     * 滑动窗口的位置                最大值
+     * ---------------               -----
+     * [1  3  -1] -3  5  3  6  7       3
+     *  1 [3  -1  -3] 5  3  6  7       3
+     *  1  3 [-1  -3  5] 3  6  7       5
+     *  1  3  -1 [-3  5  3] 6  7       5
+     *  1  3  -1  -3 [5  3  6] 7       6
+     *  1  3  -1  -3  5 [3  6  7]      7
+     *
+     * 示例 2：
+     * 输入：nums = [1], k = 1
+     * 输出：[1]
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i <k ; i++) {
+            while(!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+        }
+        int[] result = new int[nums.length-k+1];
+        result[0] = nums[queue.peekFirst()];
+        for (int i = k; i <nums.length ; i++) {
+            while(!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]){
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+            while(queue.peekFirst() <= i-k){
+                queue.pollFirst();
+            }
+            result[i-k+1] = nums[queue.peekFirst()] ;
+        }
+        return result;
+    }
+
+
+
+
+}
