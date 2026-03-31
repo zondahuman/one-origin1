4d1d93a7b40a7bcff04b5f967ee49d47a073668e
zondahuman <zondahuman@gmail.com>
2026-03-31 13:54:07 +0800

    add

---
+++ src/test/java/com/abin/algo/review/l2603/l0330/L80.java
@@ -0,0 +1,52 @@
+package com.abin.algo.review.l2603.l0330;
+
+/**
+ * 80. 删除有序数组中的重复项 II
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/description/
+ */
+public class L80 {
+
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     *
+     * 示例 1：
+     * 输入：nums = [1,1,1,2,2,3]
+     * 输出：5, nums = [1,1,2,2,3]
+     * 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3。 不需要考虑数组中超出新长度后面的元素。
+     *
+     * 示例 2：
+     * 输入：nums = [0,0,1,1,1,1,2,3,3]
+     * 输出：7, nums = [0,0,1,1,2,3,3]
+     * 解释：函数应返回新长度 length = 7, 并且原数组的前七个元素被修改为 0, 0, 1, 1, 2, 3, 3。不需要考虑数组中超出新长度后面的元素。
+     *
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int slow=0,fast=0;
+        int count=0;
+        while(fast<n){
+            if(nums[slow] != nums[fast]){
+                // 此时，对于 nums[0..slow] 来说，nums[fast] 是一个新的元素，加进来
+                slow++;
+                nums[slow] = nums[fast] ;
+            }else if(slow<fast && count<2){
+                // 此时，对于 nums[0..slow] 来说，nums[fast] 重复次数小于 2，也加进来
+                slow++;
+                nums[slow] = nums[fast] ;
+            }
+            fast++;
+            count++;
+            if(fast<n && nums[fast] != nums[fast-1]){
+                // fast 遇到新的不同的元素时，重置 count
+                count=0;
+            }
+        }
+        return slow+1;
+    }
+
+
+
+}
